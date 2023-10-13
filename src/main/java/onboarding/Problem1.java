cada644cd7a50ec6fe7d6f8fdd65b40bbb3bd467
junsoo <choix712@umn.edu>
2023-10-13 15:34:13 +0900

    [feat]: problem1 입력값이 올바른지 확인해주는 기능

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,33 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        return answer;
+
+        if (validatePage(pobi) || validatePage(crong)) {
+            answer = -1;
+            return answer;
+        }
+
+        return 0;
+
+    }
+
+    private static boolean validatePage(List<Integer>person) {
+        int leftPage = person.get(0);
+        int rightPage = person.get(1);
+        if (leftPage < 1 || rightPage > 400) {
+            return false;
+        }
+        if (rightPage <= leftPage) {
+            return false;
+        }
+        if (rightPage - leftPage != 1) {
+            return false;
+        }
+        return true;
     }
 }
