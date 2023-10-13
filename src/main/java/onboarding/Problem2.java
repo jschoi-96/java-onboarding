68b5e9b80c1240cf80d3c9a54145256bc890d399
junsoo <choix712@umn.edu>
2023-10-13 19:01:49 +0900

    feat: 연속으로 중복된 문자열이 있는지 찾는 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,13 +1,28 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
 
         if (!validateInput(cryptogram)) {
-            answer = "올바른 입력값이 아닙니다.";
+            return "올바른 입력값이 아닙니다.";
         }
-        return answer;
+
+    }
+
+
+
+    private static boolean findConsecutiveDuplicate(StringBuilder cryptogram) {
+        for (int i = 1; i < cryptogram.length(); i++){
+            if (cryptogram.charAt(i-1) == cryptogram.charAt(i)){
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean validateInput(String cryptogram) {
