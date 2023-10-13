7335aba1ff05c266295bd05cf20a36fbdf0cd24b
junsoo <choix712@umn.edu>
2023-10-13 16:29:53 +0900

    [docs] 입력값이 예외 사항일 경우 체크하는 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,6 +3,29 @@ package onboarding;
 public class Problem2 {
     public static String solution(String cryptogram) {
         String answer = "answer";
+
+        if (!validateInput(cryptogram)) {
+            answer = "올바른 입력값이 아닙니다.";
+        }
         return answer;
+    }
+
+    private static boolean validateInput(String cryptogram) {
+        if (cryptogram.length() < 1 || cryptogram.length() > 1000) {
+            return false;
+        }
+
+        for(int i = 0; i < cryptogram.length(); i++){
+            char eachLetter = cryptogram.charAt(i);
+
+            if (!Character.isAlphabetic(eachLetter)) {
+                return false;
+            }
+
+            if (Character.isUpperCase(eachLetter)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
