144dd466cc718298024da67121ca8568acfc8374
junsoo <choix712@umn.edu>
2023-10-13 19:19:51 +0900

    docs: 각 자리의 숫자가 3,6,9중 하나인지 판단하고 값을 추가해주는 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,22 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 0; i <= number; i++){
+            answer += containsValue(i);
+        }
         return answer;
+    }
+
+    public static int containsValue(int number) {
+        String numberString = String.valueOf(number);
+        int result = 0;
+        for(int i = 0; i < numberString.length(); i++){
+            char letter = numberString.charAt(i);
+            int isClap = Character.getNumericValue(letter);
+            if (isClap == 3 || isClap == 6 || isClap == 9) {
+                result++;
+            }
+        }
+        return result;
     }
 }
