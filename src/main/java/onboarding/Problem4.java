fd9afdc66b7bddc002017f1db306f9eacf57d6be
junsoo <choix712@umn.edu>
2023-10-14 02:14:23 +0900

    docs: 예외처리 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,24 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        if (validateInput(word)){
+            return "입력값을 다시 입력해주십시오.";
+        }
         return answer;
+    }
+
+
+    private static boolean validateInput(String word) {
+        if (word.length() < 1 || word.length() > 1000){
+            return true;
+        }
+
+        for(int i = 0; i < word.length(); i++){
+            if (!Character.isAlphabetic(word.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
