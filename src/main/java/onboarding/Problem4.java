1ad873993309f2e3f7a8b20d8d1bdb8aa5fed8fa
junsoo <choix712@umn.edu>
2023-10-14 02:22:11 +0900

    feat: 주어진 문자를 반대로 바꾸는 기능을 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,22 +4,34 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
-        if (validateInput(word)){
-            return "입력값을 다시 입력해주십시오.";
-        }
-        return answer;
+
+        return reverseWord(word);
     }
 
+    private static String reverseWord(String word){
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < word.length(); i++){
+            char letter = word.charAt(i);
+            if (Character.isUpperCase(letter)) {
+                char reversedUpper = (char) ('Z' - (letter - 'A'));
+                result.append(reversedUpper);
+            }
+
+            else if (Character.isLowerCase(letter)){
+                char reversedLower = (char)('z' - (letter - 'a'));
+                result.append(reversedLower);
+            }
+
+            else {
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
 
     private static boolean validateInput(String word) {
         if (word.length() < 1 || word.length() > 1000){
             return true;
-        }
-
-        for(int i = 0; i < word.length(); i++){
-            if (!Character.isAlphabetic(word.charAt(i))) {
-                return true;
-            }
         }
         return false;
     }
