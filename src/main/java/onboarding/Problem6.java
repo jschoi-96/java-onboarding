f0eb3bb3fa25154e7473a54e389edb07253af480
junsoo <choix712@umn.edu>
2023-10-15 17:29:44 +0900

    feat: 중복된 닉네임 Set에 반환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,31 +1,39 @@
 package onboarding;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        HashSet<String> result = new HashSet<>();
+
+        for(int i = 0; i < forms.size(); i++){
+            for(int j = i + 1; j < forms.size(); j++){
+                result.addAll(addDuplicate(forms.get(i), forms.get(j)));
+            }
+        }
+        answer = List.copyOf(result);
+
         return answer;
     }
 
     private static boolean isDuplicate(String nickname, String target) {
         for(int i = 1; i < nickname.length(); i++){
             String compare = nickname.substring(i-1, i+1);
-            if (compare.contains(target)){
+            if (target.contains(compare)){
                 return true;
             }
         }
         return false;
     }
 
-    private static List<String> addDuplicate(List<String> crew, List<String> compareCrew) {
-        List<String> result = new ArrayList<>();
+    private static Set<String> addDuplicate(List<String> crew, List<String> compareCrew) {
+        Set<String> result = new HashSet<>();
         String nickname = crew.get(1);
         String target = compareCrew.get(1);
         if (isDuplicate(nickname,target)) {
             result.add(crew.get(0));
-            result.add(crew.get(1));
+            result.add(compareCrew.get(0));
         }
         return result;
     }
