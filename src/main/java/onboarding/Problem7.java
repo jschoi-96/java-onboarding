c3b360fe40e46cd7a7c2e87973ba64c3aa17322b
junsoo <choix712@umn.edu>
2023-10-15 18:26:03 +0900

    feat: user의 친구를 List에 저장 하는 기능

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
+
+        userFriend(user , friends);
         return answer;
     }
+
+    private static List<String> userFriend(String user, List<List<String>> friends) {
+        List<String> result = new ArrayList<>();
+        for(int i = 0; i < friends.size(); i++){
+            String A = friends.get(i).get(0);
+            String B = friends.get(i).get(1);
+            if (A.equals(user)) {
+                result.add(B);
+            }
+
+            else if (B.equals(user)){
+                result.add(A);
+            }
+        }
+        return result;
+    }
+
 }
