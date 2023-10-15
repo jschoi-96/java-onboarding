47e6fbc5a775df145b4e5b8213c8ad1520286f33
junsoo <choix712@umn.edu>
2023-10-15 17:01:37 +0900

    feat: 두 닉네임에 연속된 글자가 있는지 확인하는 기능

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,4 +7,14 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean isDuplicate(String nickname, String target) {
+        for(int i = 1; i < nickname.length(); i++){
+            String compare = nickname.substring(i-1, i+1);
+            if (compare.contains(target)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
